f28915ed84d7a22f56e7538711002f900eee333c
Alexei_Y <alexei.yarosh.web@gmail.com>
2020-12-02 22:07:01 +0300

    init surf

---
+++ #src/js/script.js
@@ -0,0 +1,20 @@
+const mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  effect: 'fade',
+  speed: 800,
+  
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
